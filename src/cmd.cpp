fa19121e86800ed787b1825c5bdfa1b9fb390500
dg4l <79771590+dg4l@users.noreply.github.com>
2025-12-04 13:04:44 -0500

    some updates

--- src/cmd.cpp
+++ src/cmd.cpp
@@ -6,36 +6,44 @@
 #include "common_include.hpp"
 #define MAX_URL_LEN 8000
 
+
+// TODO:
+// in the future we will likely serialize responses into something like json,
+// and it will be the client's job to deserialize and make it human readable.
+
 bool cmd_remove_torrent(ServerContext* ctx, ResponseContext* response, unsigned int idx) {
     if (DEBUG) {
         printf("issued remove torrent command on idx %u\n", idx);
     }
     std::vector<lt::torrent_handle> handles = ctx->session->get_torrents();
-    if (idx < handles.size()) {
-        ctx->session->remove_torrent(handles[idx]);
+    if (idx >= handles.size()) {
+        response->message = "ERROR: invalid index";
+        return false;
     }
-    response->message = "REMOVED";
+    ctx->session->remove_torrent(handles[idx]);
+    response->message = "removed";
     return true;
 }
 
 // this is real bad.
+// likely rewrite the entire function.
+#define URL_OFFSET 10
 bool parse_add_packet(char* packet, ParsedAddPacket* parsed_packet) {
     uint16_t url_len = (uint8_t)packet[9] | ((uint8_t)packet[8] << 8);
     uint16_t save_path_len = 0;
-    if (!url_len) return false;
-    else if (url_len > MAX_PACKET_SIZE - 10) return false;
-    parsed_packet->url = std::string(packet + 10, packet + 10 + url_len);
-    if (url_len == MAX_PACKET_SIZE - 10) {
+    if (!url_len || url_len > MAX_PACKET_SIZE - 10) return false;
+    if (12 + url_len > MAX_PACKET_SIZE) return false;
+    parsed_packet->url = std::string(packet + URL_OFFSET, packet + URL_OFFSET + url_len);
+    if (url_len == MAX_PACKET_SIZE - URL_OFFSET) {
         parsed_packet->save_path = ".";
     }
-    else{
-        if (12 + url_len > MAX_PACKET_SIZE) return false;
-        save_path_len = (uint8_t)packet[11 + url_len] | (uint8_t)(packet[10 + url_len]) << 8;
+    else {
+        save_path_len = (uint8_t)packet[url_len + URL_OFFSET + 1] | (uint8_t)(packet[url_len + URL_OFFSET]) << 8;
         if (!save_path_len) {
             parsed_packet->save_path = ".";
         }
-        else{
-            if (12 + url_len + save_path_len > MAX_PACKET_SIZE) return false;
+        else {
+            if (save_path_len > MAX_PACKET_SIZE - url_len - 12) return false;
             parsed_packet->save_path = std::string(packet + 12 + url_len, packet + 12 + url_len + save_path_len);
         }
     }
@@ -102,9 +110,11 @@ bool cmd_resume_idx(ServerContext* ctx, ResponseContext* response, unsigned int 
     std::vector<lt::torrent_handle> handles = ctx->session->get_torrents();
     if (idx < handles.size()) {
         handles[idx].resume();
+        response->message = "RESUMED";
+        return true;
     }
-    response->message = "RESUMED";
-    return true;
+    response->message = "FAILED WTF WTF";
+    return false;
 }
 
 bool cmd_invalid(ServerContext* ctx, ResponseContext* response) {
@@ -117,6 +127,10 @@ bool cmd_pause_all(ServerContext* ctx, ResponseContext* response) {
         printf("pause all cmd\n");
     }
     std::vector<lt::torrent_handle> handles = ctx->session->get_torrents();
+    if (handles.empty()) {
+        response->message = "ERROR: NO TORRENTS TO PAUSE!";
+        return false;
+    }
     for (size_t i = 0; i < handles.size(); ++i) {
         handles[i].pause();
     }
@@ -156,11 +170,6 @@ bool cmd_status(ServerContext* ctx, ResponseContext* response) {
     return true;
 }
 
-// todo: implement
-bool cmd_query_stats_alltime(ServerContext* ctx) {
-    return true;
-}
-
 // for current session
 bool cmd_query_stats(ServerContext* ctx, ResponseContext* response) {
     if (DEBUG) {
@@ -184,52 +193,52 @@ bool cmd_query_stats(ServerContext* ctx, ResponseContext* response) {
     return true;
 }
 
+// TODO: handle errors in a better way.
 bool handle_command(ServerContext* ctx, int client_fd, char* packet) {
     ResponseContext response;
     response.message = "default";
-    bool ret = false;
+    bool success = false;
     uint16_t command = packet[3] | (packet[2] << 8);
     switch(command) {
         case CMD_ADD:
-            ret = cmd_add_torrent(ctx, &response, packet);
+            success = cmd_add_torrent(ctx, &response, packet);
             break;
         case CMD_QUERY_STATUS:
-            ret = cmd_status(ctx, &response);
+            success = cmd_status(ctx, &response);
             break;
         case CMD_PAUSE_ALL:
-            ret = cmd_pause_all(ctx, &response);
+            success = cmd_pause_all(ctx, &response);
             break;
         case CMD_RESUME_ALL:
-            ret = cmd_resume_all(ctx, &response);
+            success = cmd_resume_all(ctx, &response);
             break;
-        case CMD_PAUSE_IDX:{
+        case CMD_PAUSE_IDX: {
             uint32_t idx = extract_idx_from_packet(packet);
-            ret = cmd_pause_idx(ctx, &response, idx);
-            break;
-        }
-        case CMD_RESUME_IDX:{
-            uint32_t idx = extract_idx_from_packet(packet);
-            ret = cmd_resume_idx(ctx, &response, idx);
+            success = cmd_pause_idx(ctx, &response, idx);
             break;
         }
-        case CMD_REMOVE_TORRENT:{
+        case CMD_RESUME_IDX: {
             uint32_t idx = extract_idx_from_packet(packet);
-            ret = cmd_remove_torrent(ctx, &response, idx);
+            success = cmd_resume_idx(ctx, &response, idx);
             break;
         }
-        // todo: impl
-        case CMD_QUERY_TORRENT_STATUS:{
+        case CMD_REMOVE_TORRENT: {
             uint32_t idx = extract_idx_from_packet(packet);
-            ret = cmd_query_torrent_status(ctx, &response, idx);
+            success = cmd_remove_torrent(ctx, &response, idx);
+            break;
+        }
+        case CMD_QUERY_TORRENT_STATUS: {
+            uint32_t idx = extract_idx_from_packet(packet);
+            success = cmd_query_torrent_status(ctx, &response, idx);
             break;
         }
         case CMD_QUERY_STATS:
-            ret = cmd_query_stats(ctx, &response);
+            success = cmd_query_stats(ctx, &response);
             break;
         default:
-            ret = cmd_invalid(ctx, &response);
+            success = cmd_invalid(ctx, &response);
             break;
     }
     send(client_fd, response.message.c_str(), strlen(response.message.c_str()), 0);
-    return ret;
+    return success;
 }
